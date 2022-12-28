f662c1a2b19a19b5313f093119326487056db9a3
Wamaitha <wamaithaweru19@gmail.com>
2022-12-28 13:44:11 +0300

    Create an array for blog posts

---
+++ src/data/thamesData.js
@@ -0,0 +1,92 @@
+import img1 from "../assets/images/1.jpg";
+import img2 from "../assets/images/2.jpg";
+import img3 from "../assets/images/3.jpg";
+import img4 from "../assets/images/4.jpg";
+import img5 from "../assets/images/5.jpg";
+import img6 from "../assets/images/6.jpg";
+import img7 from "../assets/images/7.jpg";
+import img8 from "../assets/images/8.jpg";
+import img9 from "../assets/images/9.jpg";
+import img10 from "../assets/images/10.jpg";
+import img11 from "../assets/images/11.jpg";
+import img12 from "../assets/images/12.jpg";
+import img13 from "../assets/images/13.jpg";
+
+
+
+export const themesdata = [
+    {
+        name:"",
+        type:"web",
+        img:img1,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"web",
+        img:img3,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"web",
+        img:img3,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"nocode",
+        img:img4,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"nocode",
+        img:img5,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"nocode",
+        img:img6,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"marketing",
+        img:img7,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"marketing",
+        img:img8,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"marketing",
+        img:img9,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"training",
+        img:img10,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"training",
+        img:img11,
+        heading:"",
+    },
+    {
+        name:"",
+        type:"training",
+        img:img12,
+        heading:"",
+    },
+   
+
+]
